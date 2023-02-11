2b67043a7f25a3ba0d427b6f49c6c3672be76df5
Dilip kumar <115738380+Mr-DilipKumar@users.noreply.github.com>
2023-02-11 12:55:19 +0000

    Created some files

---
+++ 3_program.js
@@ -0,0 +1,13 @@
+console.log("javscript tutorial 3: var, let and const")
+
+// var a = 45;
+// var b = "Harry";
+// var c = null
+// var d = undefined
+const author = "Dilip";
+
+{
+let b = 'this'
+console.log(b)
+}
+console.log(b)
