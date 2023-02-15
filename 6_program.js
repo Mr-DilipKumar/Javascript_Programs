fcb075e95214990d3ede84beb2b4c22306930d9c
Dilip kumar <115738380+Mr-DilipKumar@users.noreply.github.com>
2023-02-15 06:16:19 +0000

    added new file

--- 6_program.js
+++ 6_program.js
@@ -3,4 +3,15 @@ let a = 45
 let b = 10
 console.log("a + b", a+b)
 console.log("a - b", a-b)
+console.log("a * b", a*b)
+console.log("a/b", a/b)
 
+// Assignment operator
+let com1 = 5;
+let com2 = "5";
+console.log(com1 > com2)
+
+// Logical operators
+let x = 5;
+let y = 6;
+console.log(x>y && x==5)
